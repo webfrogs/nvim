c5a111a4e699b205b040457ef5868f6f6845f2bd
Carl Chen <ccf.developer@gmail.com>
2024-09-10 12:41:09 +0800

    feat: config lualine

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -23,6 +23,10 @@ return {
       local cmp = require('cmp')
 
       cmp.setup({
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
         sources = {
           { name = 'nvim_lsp' },
           { name = "treesitter" },
