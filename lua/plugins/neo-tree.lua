a566b6ae4ef735409aec1ff31d5125e5622ed820
Carl Chen <ccf.developer@gmail.com>
2025-03-31 10:57:27 +0800

    feat: replace nvim-tree by neo-tree

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,20 @@
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  lazy = false, -- neo-tree will lazily load itself
+  config = function()
+    vim.keymap.set("n", "<leader>t", "<Cmd>Neotree<CR>")
+    require("neo-tree").setup({
+      window = {
+        width = 30,
+      },
+    })
+  end,
+}
