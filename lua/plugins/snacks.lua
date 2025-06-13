7883157d82cf0a405fe2ab6d943d278e2cb98512
Carl Chen <ccf.developer@gmail.com>
2025-06-13 17:59:36 +0800

    feat: add snacks plugin

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,24 @@
+-- https://github.com/folke/snacks.nvim
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    bigfile = { enabled = true },
+    dashboard = { enabled = false }, -- see dashboard.lua plugin config
+    explorer = { enabled = true },
+    indent = { enabled = false },    -- use indent-blankline plugin instead.
+    input = { enabled = true },
+    picker = { enabled = true },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    scope = { enabled = true },
+    scroll = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+  },
+}
