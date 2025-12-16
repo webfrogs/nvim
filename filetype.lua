ba12f0dac9db9d21d9e76f3959d4671a9d98f1c3
Carl Chen <ccf.developer@gmail.com>
2025-12-16 19:22:00 +0800

    feat: add hyprlang file type detect support

--- filetype.lua
+++ filetype.lua
@@ -9,7 +9,7 @@ vim.filetype.add({
     xml = "xml",
     c = "c",
     go = "go",
-    h = function(path, bufnr)
+    h = function(_, bufnr)
       local first_line = vim.api.nvim_buf_get_lines(bufnr, 0, 1, true);
       for i = 1, #first_line do
         if first_line[i]:match('<iostream>') then
@@ -17,6 +17,22 @@ vim.filetype.add({
         end
       end
       return "c"
+    end,
+    conf = function(path, _)
+      -- 获取当前文件所在目录的父目录（即上一级目录）
+      local parent_of_file_dir = vim.fn.fnamemodify(path, ':h:h')
+      -- 检查该父目录中是否存在 hyprland.conf
+      local hyprland_conf = vim.fn.join({ parent_of_file_dir, 'hyprland.conf' }, '/')
+      if vim.fn.filereadable(hyprland_conf) == 1 then
+        return "hyprlang"
+      end
+      -- 继续探测上一级路径
+      parent_of_file_dir = vim.fn.fnamemodify(parent_of_file_dir, ':h')
+      hyprland_conf = vim.fn.join({ parent_of_file_dir, 'hyprland.conf' }, '/')
+      if vim.fn.filereadable(hyprland_conf) == 1 then
+        return "hyprlang"
+      end
+      return "conf"
     end,
   },
   filename = {
