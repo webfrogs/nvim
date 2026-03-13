fef25b1a2050f7613ca3c4e8961140ff822fdb8d
Carl Chen <ccf.developer@gmail.com>
2026-03-13 10:48:31 +0800

    feat: add treesitter command to sync ts parser instead of running it when treesitter plugin is loaded

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,18 +1,22 @@
+-- https://github.com/nvim-treesitter/nvim-treesitter
 return {
   "nvim-treesitter/nvim-treesitter",
+  lazy = false,
   branch = "main",
   build = ":TSUpdate",
   config = function()
-    require 'nvim-treesitter'.install {
-      "go",
-      "lua",
-      "cpp",
-      "c",
-      "python",
-      "rust",
-      "bash",
-      "markdown",
-      "markdown_inline",
-    }
+    vim.api.nvim_create_user_command('TSSync', function()
+      require 'nvim-treesitter'.install {
+        "go",
+        "lua",
+        "cpp",
+        "c",
+        "python",
+        "rust",
+        "bash",
+        "markdown",
+        "markdown_inline",
+      }
+    end, {})
   end,
 }
