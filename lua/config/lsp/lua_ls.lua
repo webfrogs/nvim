6295058bc8fff70f46e48b46aa2cf36de58a2930
Carl Chen <ccf.developer@gmail.com>
2026-05-12 15:31:57 +0800

    feat: lua ls for hyprland

--- lua/config/lsp/lua_ls.lua
+++ lua/config/lsp/lua_ls.lua
@@ -1,17 +1,21 @@
 return {
-  -- settings = {
-  --   Lua = {
-  --     diagnostics = {
-  --       globals = { "vim" },
-  --     },
-  --     workspace = {
-  --       library = {
-  --         vim.env.VIMRUNTIME,
-  --         vim.fn.expand("$VIMRUNTIME/lua"),
-  --         vim.fn.stdpath("config") .. "/lua",
-  --         vim.fn.stdpath("data") .. "/lazy/snacks.nvim/lua",
-  --       },
-  --     },
-  --   },
-  -- },
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = {
+          -- "vim",
+          "hl",
+        },
+      },
+      workspace = {
+        library = {
+          "/usr/share/hypr/stubs",
+          -- vim.env.VIMRUNTIME,
+          -- vim.fn.expand("$VIMRUNTIME/lua"),
+          -- vim.fn.stdpath("config") .. "/lua",
+          -- vim.fn.stdpath("data") .. "/lazy/snacks.nvim/lua",
+        },
+      },
+    },
+  },
 }
